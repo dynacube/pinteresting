d3de01b65a17b75e33677fb598b963dee626e319
Andrew Roland <dynacube@gmail.com>
2015-05-22 15:33:44 -0400

    Changed bucket to S3

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,8 @@ class Pin < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"}
+  #:storage => :s3
+  #:bucket => 'dyna-cube-omr'
   do_not_validate_attachment_file_type :image
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ config.paperclip_defaults = {
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
     :region => 'us-west-2'
   }
 }
